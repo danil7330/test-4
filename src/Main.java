f28dde1a7f1bab68e13a1bfc0011b80e4a5efff2
Neto test <danilvot-2011@mail.ru>
2024-02-15 13:11:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int score = 300;
+        int replen = 4000;
+        int bonus = replen / 500;
+        int balancebonus = score + replen + bonus;
+        int balance = score + replen;
+        if (replen >= 1500)
+
+        {System.out.println("Итоговый счет=" + balancebonus);
+        } else {
+        System.out.println("Итоговый счет=" + balance );}
+
+
+
+        }
+    }
